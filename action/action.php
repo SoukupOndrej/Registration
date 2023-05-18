64e3c7b218f006b669be0ac8b0c44b7883f86a7c
Ondřej Soukup <soukup.ondrej.2007@gmail.com>
2023-05-18 20:17:19 +0200

    added some basic code. stuff like password checking, checking if the data are set etc.

---
+++ action/action.php
@@ -0,0 +1,42 @@
+<?php
+
+	session_start();
+	$errors = [];
+
+	if (isset($_POST['email']) AND empty($_POST['email'])) {
+		$errors['email'] = "Fill in your email, please";
+	}
+
+	if (isset($_POST['password']) AND empty($_POST['password'])) {
+		$errors['password'] = "Fill in your password, please";
+	} else {
+		if (isset($_POST['repeatPassword']) AND empty($_POST['repeatPassword'])) {
+			$errors['repeatPassword'] = "Confirm your password, please";
+		}
+	}
+
+	if (count($errors) == 0) {
+		$password = $_POST['password'];
+		$uppercase = preg_match('@[A-Z]@', $password);
+		$lowercase = preg_match('@[a-z]@', $password);
+		$number = preg_match('@[0-9]@', $password);
+		$specialChars = preg_match('@[^\w]@', $password);
+		if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+		    $errors[] = 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+		} elseif ($_POST['password'] != $_POST['repeatPassword']) {
+			$errors[] = "Passwords don't match";
+		}
+	}
+
+	if (count($errors) > 0) {
+		$_SESSION['errors'] = $errors;
+		header("location: ../signup.php");
+	} else {
+		echo "Everything is OK";
+	}
+
+	foreach ($errors as $key => $value) {
+		echo $value;
+	}
+
+?>
--- signup.php
+++ signup.php
@@ -1,3 +1,17 @@
+<?php
+
+	session_start();
+
+	function get_error($errorArg){
+		if (isset($_SESSION['errors'][$errorArg]) AND !empty($_SESSION['errors'][$errorArg])) {
+			$ret = $_SESSION['errors'][$errorArg];
+		} else {
+			$ret = null;
+		}
+		return $ret;
+	}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,18 +26,27 @@
 			<h1>Sign Up</h1>
 		</div>
 		<div id="form">
-			<form>
+			<form action="action/action.php" method="post">
 				<div class="input_field">
 					<label for="email">Email:</label>
 					<input type="email" name="email">
+				</div>
+				<div class="error">
+					<p class="errors"><?=get_error('email')?></p>
 				</div>
 				<div class="input_field">
 					<label for="password">Password:</label>
 					<input type="text" name="password">
 				</div>
+				<div class="error">
+					<p class="errors"><?=get_error('password')?></p>
+				</div>
 				<div class="input_field">
 					<label for="repeatPassword">Repeat Password:</label>
 					<input type="text" name="repeatPassword">
+				</div>
+				<div class="error">
+					<p class="errors"><?=get_error('repeatPassword')?></p>
 				</div>
 				<div class="input_field">
 					<input type="submit" class="sbt" value="Sign Up">
